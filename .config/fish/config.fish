04850d33ec65f73fafa7738dd239145ec0322f9e
Peter Kapsiar <kapsiar888n@gmail.com>
2026-05-19 13:20:01 +0200

    customized prompt a bit

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -8,7 +8,7 @@ alias dots='git --git-dir=$HOME/.dotfiles --work-tree=$HOME'
 # fzf_configure_bindings --directory=\ec
 
 # Customize fzf fd options to include .config directory specifically
-set -gx fzf_fd_opts --hidden --exclude .git --exclude .cache --exclude .local --exclude .mozilla --exclude .npm --exclude .cargo
+set -gx fzf_fd_opts --hidden --exclude .git --exclude .cache --exclude .local --exclude .mozilla --exclude .npm --exclude .cargo --exclude .dotfiles-backup
 
 function yz
     # Create temp file for yazi to write cwd into
@@ -30,4 +30,61 @@ function yz
 
     # Clean up
     rm -f -- "$tmp"
+end
+
+function fish_prompt
+    set -l host (prompt_hostname)
+    set -l cwd (pwd)
+    if test -n "$HOME"
+        if test "$cwd" = "$HOME"; or string match -q "$HOME/*" -- $cwd
+            set cwd (string replace -r "^$HOME" "~" -- $cwd)
+        end
+    end
+    set -l git_indicator ""
+    set -l c_blue 89b4fa
+    set -l c_mauve cba6f7
+    set -l c_green a6e3a1
+    set -l c_peach fab387
+    set -l c_overlay 7f849c
+
+    if command -sq git; and command git rev-parse --is-inside-work-tree >/dev/null 2>&1
+        set -l git_branch (command git symbolic-ref --quiet --short HEAD 2>/dev/null)
+        if test -z "$git_branch"
+            set git_branch (command git rev-parse --short HEAD 2>/dev/null)
+        end
+        if test -n "$git_branch"
+            set git_indicator " (git:$git_branch)"
+        end
+    end
+
+    set_color $c_overlay
+    echo -n "┌["
+    
+    set_color $c_blue
+    echo -n $USER"@"$host
+
+    set_color $c_overlay
+    echo -n "] "
+
+    set_color $c_mauve
+    echo -n "("$cwd")"
+
+    if test -n "$git_indicator"
+        set_color $c_green
+        echo -n $git_indicator
+    end
+
+    set_color normal
+    echo
+
+    set_color $c_overlay
+    echo -n "└["
+
+    set_color $c_peach
+    echo -n '$'
+
+    set_color $c_overlay
+    echo -n "] "
+
+    set_color normal
 end
