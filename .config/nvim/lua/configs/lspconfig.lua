91c4168c1f4d72a003fbcffabc8af9b4e52a4b8f
Peter Kapsiar <kapsiar888n@gmail.com>
2026-05-21 16:16:40 +0200

    added LSPs to nvim config

--- .config/nvim/lua/configs/lspconfig.lua
+++ .config/nvim/lua/configs/lspconfig.lua
@@ -1,6 +1,17 @@
-require("nvchad.configs.lspconfig").defaults()
+local nvchad_lsp = require "nvchad.configs.lspconfig"
+nvchad_lsp.defaults()
 
-local servers = { "html", "cssls" }
+-- Add extra servers here. lua_ls already enabled by NVChad defaults.
+local servers = {
+    "pyright",
+    "tsserver",
+    "html",
+    "cssls",
+    "jsonls",
+    "bashls",
+    "clangd",
+    "jdtls",
+}
 vim.lsp.enable(servers)
 
--- read :h vim.lsp.config for changing options of lsp servers 
+-- read :h vim.lsp.config for changing options of lsp servers
