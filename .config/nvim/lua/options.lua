9171c2308b59bf5bcbb017425e554e9dd5a31f36
Peter Kapsiar <kapsiar888n@gmail.com>
2026-06-03 21:53:05 +0200

    disabled word wrap and added line width indicator

--- .config/nvim/lua/options.lua
+++ .config/nvim/lua/options.lua
@@ -5,3 +5,6 @@ require "nvchad.options"
 local o = vim.o
 o.cursorlineopt ='both' -- to enable cursorline!
 vim.opt.scrolloff = 8
+vim.opt.wrap = false -- disable word wrap
+vim.opt.textwidth = 120 -- to set preffered line length
+vim.opt.colorcolumn = "+0" -- draw a line at textwidth
